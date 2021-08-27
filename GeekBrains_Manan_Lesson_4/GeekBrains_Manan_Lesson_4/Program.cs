6b050984b9bef293d42d1632a1ae2b0d70320cb1
PaRoNoRMaLNo <73099992+PaRoNoRMaLNo@users.noreply.github.com>
2021-08-27 11:14:07 +0300

    0.1

---
+++ GeekBrains_Manan_Lesson_4/GeekBrains_Manan_Lesson_4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+namespace GeekBrains_Manan_Lesson_4
+{
+    //Маннанов
+    //1. Дан целочисленный массив из 20 элементов.Элементы массива могут принимать целые значения от –10 000 до 10 000 включительно.
+    //    Заполнить случайными числами.Написать программу, позволяющую найти и вывести количество пар элементов массива, в которых только одно число делится на 3.
+    //    В данной задаче под парой подразумевается два подряд идущих элемента массива.
+    class Program
+    {
+        static short Check_del(short[] mas)
+        {
+            short count = 0;
+            for (int i = 0; i < mas.Length - 1; i++)
+            {
+                if (mas[i] % 3 == 0 || mas[i + 1] % 3 == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            short[] mas = new short[20];
+            Random random = new Random();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = (short)random.Next(-10000, 10001);
+                Console.WriteLine($"{mas[i]}");
+            }
+            Console.WriteLine("Количество пар: " + Check_del(mas));
+
+           
+            
+            Console.WriteLine("\nЧисла из файла");// б) Добавьте статический метод для считывания массива из текстового файла.Метод должен возвращать массив целых чисел;
+            List<short> list = StaticClass.ReadMasFromFile();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            //б)** Создать библиотеку содержащую класс для работы с массивом. Продемонстрировать работу библиотеки
+            Massiv.My massiv = new Massiv.My(10, 1, 3);
+            massiv.Write();
+            Console.WriteLine("Количество макс элементов = " + massiv.MaxCount);
+            Console.WriteLine("Сумма элементов = " + massiv.Sum);
+            Console.Read();
+        }
+    }
+}
---
+++ GeekBrains_Manan_Lesson_4/Lesson4/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    class Program
+    {
+        //Маннанов
+        //4. Решить задачу с логинами из урока 2, только логины и пароли считать из файла в массив.Создайте структуру Account, содержащую Login и Password.
+        struct Account
+        {
+            public string Login;
+            public string Password;
+        }
+        static bool CheckAcc(string[,] acc,Account account)
+        {
+            for (int i = 0; i < acc.Length / 2; i++)
+            {
+                if (account.Login == acc[i, 0] && account.Password == acc[i, 1])
+                    return true;
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            string[,] acc = new string[System.IO.File.ReadAllLines("Accounts.txt").Length / 2,2];
+            using (StreamReader reader = new StreamReader("Accounts.txt "))
+            {
+                string line;
+                int count = 0;
+                int row = 0;
+                while ((line = reader.ReadLine()) != null)
+                {
+
+                    if (count % 2 == 0)
+                    {
+                        acc[row, 0] = line;
+                        count++;
+                    }
+                    else
+                    {
+                        acc[row, 1] = line;
+                        count++;
+                        row++;
+                    }
+                    
+                }
+            }
+
+            Account account = new Account();
+            Console.Write("Введите Логин: ");
+            account.Login = Console.ReadLine();
+            Console.Write("Введите Пароль: ");
+            account.Password = Console.ReadLine();
+            if (CheckAcc(acc, account))
+                Console.WriteLine("Вы зашли");
+            else
+                Console.WriteLine("Неправильные данные");
+            Console.ReadKey();
+        }
+    }
+}
